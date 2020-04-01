798a633a13de296818afe2bcb7669380275241e9
Abdo <abdosalah3105@gmail.com>
2020-04-01 20:25:07 +1100

    First draft of Blog Page

--- src/themes/default/pages/Blog.vue
+++ src/themes/default/pages/Blog.vue
@@ -1,18 +1,75 @@
 <template>
-  <div>
-    <div class="">
-      <p>
+  <div class="mr-nav ml-12 mb-32">
+    <!-- PAGE TITLE -->
+    <div class="bg-blue-300">
+      <p class="py-20 text-center text-5xl font-bold">
         Blog Page
       </p>
+    </div>
+    <!-- PAGE HEADER -->
+    <div class="mt-16">
+      <p class="pb-8 text-4xl font-bold">
+        Blog Name
+      </p>
+      <p class="text-red">
+        Author Name
+      </p>
+    </div>
+    <!-- PAGE BODY -->
+    <div class="mt-16">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Cras non enim ac massa convallis dignissim vel non velit.
+        Vestibulum vehicula.
+      </p>
+      <div class="flex mt-10">
+        <div class="w-1/2 bg-blue-300">
+          <!-- HERE GOES THE IMAGES -->
+        </div>
+        <div class="w-1/2 pl-6">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Cras non enim ac massa convallis dignissim vel non velit.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Cras non enim ac massa convallis dignissim vel non velit.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Cras non enim ac massa convallis dignissim vel non velit.
+          </p>
+          <p class="mt-6">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Cras non enim ac massa convallis dignissim vel non velit.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Cras non enim ac massa convallis dignissim vel non velit.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Cras non enim ac massa convallis dignissim vel non velit.
+          </p>
+          <p class="mt-6">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Cras non enim ac massa convallis dignissim vel non velit.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Cras non enim ac massa convallis dignissim vel non velit.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Cras non enim ac massa convallis dignissim vel non velit.
+          </p>
+        </div>
+      </div>
+      <!-- BUTTONS -->
+      <div class="flex mt-12">
+        <button class="w-1/2 mx-8 bg-blue-300 py-4">
+          Back to Articles
+        </button>
+        <button class="w-1/2 mx-8 bg-blue-300 py-4">
+          Browse
+        </button>
+      </div>
+      <!-- CAROUSEL -->
+      <!-- TBD -->
     </div>
   </div>
 </template>
 
 <style>
 footer {
-  display: none;
-}
-nav {
   display: none;
 }
 </style>
